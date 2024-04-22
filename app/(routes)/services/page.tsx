34518e96ffeffab1026b0d7951ab6245a3b0983a
CariniGuido <103467691+CariniGuido@users.noreply.github.com>
2024-04-21 23:46:30 -0300

    estilos

--- app/(routes)/services/page.tsx
+++ app/(routes)/services/page.tsx
@@ -11,10 +11,12 @@ const ServicesPage = () => {
             <CircleImage />
             <AvatarServices />
             <div className="flex items-center justify-center h-screen max-w-5xl gap-6 mx-auto md:grid-cols-2">
-                <div className="max-w-[450px] ">
-                    <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5 mb-5">Mis <span className="font-bold text-secondary">servicios.</span></h1>
-                    <p className="mb-0 md:mb-3 text-xl text-gray-300">Ofrezco servicios de desarrollo web frontend especializados en la creación de sitios web y aplicaciones atractivas y funcionales. Utilizando las últimas tecnologías, como HTML, CSS ,JavaScript, React-js , Node, Boostrap, Css tailwind diseño interfaces de usuario intuitivas y responsivas que reflejan la identidad de marca de mis clientes y mejoran su presencia en línea.</p>
-                    <button className="px-3 py-2 rounded-lg bg-secondary hover:bg-secondary/65">Contacta conmigo</button>
+                <div className="max-w-[450px] mt-10">
+                   <div  >
+                   <h1 className="text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-5 mb-5" style={{ marginTop: '65px' }}>Mis <span className="font-bold text-secondary"  >servicios.</span></h1>
+                    <p className="mb-0 md:mb-3 m-2 text-xl text-gray-300">Ofrezco servicios de desarrollo web frontend especializados en la creación de sitios web y aplicaciones atractivas y funcionales. Utilizando las últimas tecnologías, como HTML, CSS ,JavaScript, React-js , Node, Boostrap, Css tailwind diseño interfaces de usuario intuitivas y responsivas que reflejan la identidad de marca de mis clientes y mejoran su presencia en línea.</p>
+                    <button className="mt-12 px-3 py-2 rounded-lg bg-secondary hover:bg-secondary/65">Contacta conmigo</button>
+                    </div>
                 </div>
 
  </div>
--- app/(routes)/testimonials/page.tsx
+++ app/(routes)/testimonials/page.tsx
@@ -16,11 +16,11 @@ const TestimonialsPage = () => {
             <TransitionPage />
             <div className='flex flex-col justify-center h-lvh'>
                 <CircleImage />
-                <h1 className="text-2xl leading-tight text-center md:text-4xl md:mb-5">
+                <h1 className="text-2xl leading-tight text-center md:text-4xl md:mb-5 mt-13" style={{ marginTop: 'auto' }}>
                     Algunos comentarios
                     <span className="block font-bold text-secondary"> de nuestros clientes</span>
                 </h1>
-                <div className="flex items-center justify-center">
+                <div className="flex items-center justify-center mt-8">
                     <div>
                         <Swiper
                             breakpoints={{
